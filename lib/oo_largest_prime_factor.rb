c0bb87bbb5c06759ef57337c3785329d5200c4fc
dwandrew <danwandrew@gmail.com>
2020-11-16 14:34:03 +0000

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+attr_accessor :num
+
+def initialize(num)
+    @num = num
+end
+
+def number
+    division = Prime.prime_division(self.num)
+    division[-1][0]
+end
+
+
+end
